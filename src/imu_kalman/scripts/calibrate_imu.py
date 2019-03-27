09313252aaf91c7107c738a65b77ca6dd9101fea
Austin Lamb <arlamb@usc.edu>
2019-03-26 19:02:14 -0700

    Added calibration code for imu and integrated with imunode

---
+++ src/imu_kalman/scripts/calibrate_imu.py
@@ -0,0 +1,43 @@
+from geometry_msgs.msg import Vector3Stamped
+import rospy
+
+count = 0
+acceleration_bias = Vector3Stamped()
+accel_zero_count = 0
+
+def addAccelSample(acceleration):
+	acceleration_bias.vector.x += acceleration.vector.x
+	acceleration_bias.vector.y += acceleration.vector.y
+	acceleration_bias.vector.z += acceleration.vector.z
+	count += 1
+
+def calibrate():
+	acceleration_bias.vector.x = 0
+	acceleration_bias.vector.y = 0
+	acceleration_bias.vector.z = 0
+	subscription = rospy.Subscriber('ngimu/earthAccel', Vector3Stamped, addAccelSample)
+	
+	while True:
+		if count >= 1024:
+			subscription.unregister()
+			
+	acceleration_bias.vector.x /= count
+	acceleration_bias.vector.y /= count
+	acceleration_bias.vector.z /= count
+
+	return acceleration_bias
+
+def checkAccelZero(acceleration):
+	if acceleration.vector.x == 0 and acceleration.vector.y == 0 and acceleration.vector.z == 0:
+		accel_zero_count += 1
+	else:
+		accel_zero_count = 0
+
+def movement_end_check(acceleration_bias):
+	pub = rospy.Publisher('ngimu/zeroVelocity', bool, queue_size=10)
+	rospy.Subscriber('ngimu/earthAccel', Vector3Stamped, checkAccelZero)
+	rate = rospy.Rate(10)
+
+	while not rospy.is_shutdown():
+		pub.publish(True)
+		rate.sleep()
